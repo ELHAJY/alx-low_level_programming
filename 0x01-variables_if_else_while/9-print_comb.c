223eab0b1758661e58617cef65c51bc1eac7c4fd
ELHAJY <elhajydriss@gmail.com>
2023-07-15 07:07:55 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0=success
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+return (0);
+}
