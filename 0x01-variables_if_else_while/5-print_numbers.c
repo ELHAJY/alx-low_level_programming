af2655a3c45b4d8f2ccfdd0fbfcb99134a71309d
ELHAJY <elhajydriss@gmail.com>
2023-07-15 06:21:56 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0=success
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10 ; i++)
+		printf("%i", i);
+	printf("\n");
+return (0);
+}
