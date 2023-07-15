557fa054d79e4ba213ec0539d83086a624114b93
ELHAJY <elhajydriss@gmail.com>
2023-07-15 05:46:24 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: 0=success
+ */
+
+int main(void)
+{
+	char alph = 'a';
+
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	alph = A;
+	while (alph <= 'Z')
+	{
+		putchar(alph);
+		alph++;
+	}
+return (0);
+}
