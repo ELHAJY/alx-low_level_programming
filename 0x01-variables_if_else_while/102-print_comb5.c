1ce2e6d9d725a23d3de923d24d0264963b6a4fa4
ELHAJY <elhajydriss@gmail.com>
2023-07-15 10:00:23 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,16 +12,16 @@ int main(void)
 {
 	int d1 = 0, d2, d3, d4;
 
-	while (d1 <= 1)
+	while (d1 <= 9)
 	{
 		d2 = 0;
-		while (d2 <= 1)
+		while (d2 <= 9)
 		{
 			d3 = d1;
-			while (d3 <= 1)
+			while (d3 <= 9)
 			{
 				d4 = 0;
-				while (d4 <= 2)
+				while (d4 <= 9)
 				{
 					if (d2 >= d4 && d3 == d1)
 					d4 = d2 + 1;
