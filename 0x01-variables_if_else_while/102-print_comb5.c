bc04552f6d4fa4b798fb9af44cdbfcb67c7835ec
ELHAJY <elhajydriss@gmail.com>
2023-07-15 10:06:58 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -30,7 +30,7 @@ int main(void)
 					putchar(' ');
 					putchar(d3 + '0');
 					putchar(d4 + '0');
-					if (d1 != 9 ||  d2 != 8 || d3 != 9 || d4 != 9 ||)
+					if (d1 != 9 ||  d2 != 8 || d3 != 9 || d4 != 9)
 					{
 						putchar(',');
 						putchar(' ');
