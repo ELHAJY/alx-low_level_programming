493ac986abff7be6235cc1f6c2ae34c1942bb270
ELHAJY <elhajydriss@gmail.com>
2023-07-15 09:35:45 -0700

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,38 +12,38 @@ int main(void)
 {
 	int d1, d2, d3, d4;
 
-		while (d1 <= 9)
+	while (d1 <= 9)
+	{
+		while (d2 <= 9)
 		{
-			while (d2 <= 9)
+			if (d1 <= d3)
 			{
-				if (d1 <= d3)
+				while (d3 <= 9)
 				{
-					while (d3 <= 9)
+					d4 = 0;
+					while (d4 <= 9)
 					{
-						d4 = 0;
-						while (d4 <= 9)
-						{
-							if (d2 >= d4)
-							d4 = d2 + 1;
-							putchar(d1 + '0');
-							putchar(d2 + '0');
-							putchar(' ');
-							putchar(d3 + '0');
-							putchar(d4 + '0');
-							if (d1 + d2 + d3 + d4 < 35)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-							d4++;
-						}
-						d3++;
+					if (d2 >= d4)
+						d4 = d2 + 1;
+					putchar(d1 + '0');
+					putchar(d2 + '0');
+					putchar(' ');
+					putchar(d3 + '0');
+					putchar(d4 + '0');
+					if (d1 + d2 + d3 + d4 < 35)
+					{
+						putchar(',');
+						putchar(' ');
 					}
+						d4++;
+					}
+					d3++;
 				}
-				d2++;
 			}
-			d1++;
+			d2++;
 		}
+		d1++;
+	}
 
 return (0);
 }
