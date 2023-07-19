bef88599e62fdd36ace1a431476a522864e41487
ELHAJY <elhajydriss@gmail.com>
2023-07-19 13:28:01 -0700

    alx-low_level_programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: 0=success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+
+
+return (0);
+}
