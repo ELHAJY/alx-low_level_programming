e259efbb50eb1703b70dbfcf02edb8fdd5b0e3f4
ELHAJY <elhajydriss@gmail.com>
2023-07-19 13:00:40 -0700

    alx-low_level_programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar, followed by a new line.
+ *
+ * Return: 0=success
+ */
+
+int main(void)
+{
+	char ch[] = "_putchar";
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(ch[]);
+	_putchar('\n');
+
+return (0);
+}
+
