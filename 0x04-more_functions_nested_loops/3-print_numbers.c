5402a06b77eb294d413a7563ec206bae107c05a7
ELHAJY <elhajydriss@gmail.com>
2023-07-20 14:37:17 -0700

    alx-low_level_programming

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers
+ *
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar('0' + x);
+	_putchar('\n');
+}
