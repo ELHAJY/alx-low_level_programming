353425d6fa5e59ff0a560ef6c48c8bffd6b0b18e
ELHAJY <elhajydriss@gmail.com>
2023-07-20 15:07:48 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * more_numbers - function that prints the numbers
+ * more_numbers - Write a function that prints 10 times the numbers, from 0 to 14
  *
  */
 
 void more_numbers(void)
 {
-	int x;
+	int x, i, n;
 
 	for (x = 0; x < 10; x++)
 	{
-		int i;
-
 		for (i = 0; i < 10; i++)
 			_putchar('0' + i);
-		for (i = 0; i <= 4; i++)
+		for (n = 0; n <= 4; n++)
 		{
 			_putchar('1');
 			_putchar('0' + i);
