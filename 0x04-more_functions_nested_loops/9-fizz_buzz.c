3f90b9fc93d57a61d341603b55b3b2be68eaedba
ELHAJY <elhajydriss@gmail.com>
2023-07-20 18:10:20 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,15 +2,11 @@
 
 /**
  * main - program that prints the numbers from 1 to 100,
- *	followed by a new line. 
- *	But for multiples of three print Fizz instead of the number
- *	and for the multiples of five print Buzz. 
- *	For numbers which are multiples of both three and five print FizzBuzz.
  *
  * Return: always 0
 */
 
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -18,15 +14,13 @@ int main (void)
 	{
 		if (n % 15 == 0)
 			printf("FizzBuzz");
-		else
-		{
-			if (n % 3 == 0)
+		else if (n % 3 == 0)
 				printf("Fizz");
-			else if (n % 5 == 0)
-				printf("Buzz");
-			else
-				printf("%i", n);
-		}
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", n);
+
 		if (n != 100)
 			printf(" ");
 		else
