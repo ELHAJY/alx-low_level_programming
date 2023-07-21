41dc22dd491d44e48533dd182ac2502c2802c601
ELHAJY <elhajydriss@gmail.com>
2023-07-21 12:21:48 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: imput integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int v;
+
+	if (n < 0)
+	{
+		v = -n;
+		_putchar('-');
+	} else
+	{
+		v = n;
+	}
+
+	if (v / 10)
+	{
+		print_number(v / 10);
+	}
+
+	_putchar((v % 10) + '0');
+}
