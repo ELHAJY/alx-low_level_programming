86b5b8e2bdf8f6a30fa692c036d89d89994f354f
ELHAJY <elhajydriss@gmail.com>
2023-07-20 19:02:19 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - program that prints the numbers from 1 to 100,
@@ -26,5 +26,4 @@ int main(void)
 		else
 			printf("\n");
 	}
-	return (0);
 }
