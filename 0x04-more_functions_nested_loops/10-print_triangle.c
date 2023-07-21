0cfd7531f753e3025eb76f29706a597ae57fea31
ELHAJY <elhajydriss@gmail.com>
2023-07-20 18:48:11 -0700

    alx-low_level_programming

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,21 +3,21 @@
 /**
  * print_triangle - function that prints a square, followed by a new line.
  *
- * @size: is the size of the square
+ * @size: is the size of the triangle
  */
 
 void print_triangle(int size)
 {
-        int s, q;
+	int s, q, v;
 
-        for (s = 1; s <= size; s++)
-        {
+	for (s = 1; s <= size; s++)
+	{
 		for (v = size; v > 1; v--)
 			_putchar(' ');
-                for (q = 1; q <= s; q++)
-                        _putchar(35);
-                _putchar('\n');
-        }
-        if (size <= 0)
-                _putchar('\n');
+		for (q = 1; q <= s; q++)
+			_putchar(35);
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
 }
