9cdf94a9b7fb00a11fbd45f8b4d7e671f8fa8a87
ELHAJY <elhajydriss@gmail.com>
2023-07-25 16:27:26 -0700

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,11 +15,11 @@ int _atoi(char *s)
 	do
 	{
 		if (*s == '-')
-			sing *= -1;
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
 		else if (n > 0)
 			break;
 	} while (*s++);
-	return (sing * n);
+	return (sign * n);
 }
