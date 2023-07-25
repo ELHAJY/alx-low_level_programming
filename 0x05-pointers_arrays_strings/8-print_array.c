3a25c33b04e5c47242274a6b9b6c95d324141224
ELHAJY <elhajydriss@gmail.com>
2023-07-25 06:58:58 -0700

    alx-low_level_programming

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array of integers.
+ *
+ * @a: array imput
+ * @n: the number of elements of the array to be printed.
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(a[i]);
+	_putchar('\n');
+
+}
