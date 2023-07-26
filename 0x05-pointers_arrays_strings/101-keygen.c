29cc5713d8bab040a39bffd69f3bd98dae8bfdaf
ELHAJY <elhajydriss@gmail.com>
2023-07-25 17:06:03 -0700

    alx-low_level_programming

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * main - program that generates random.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int s;
+	char ch;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		ch = rand() % 128;
+		s = s + c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
