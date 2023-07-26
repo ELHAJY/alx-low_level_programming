49314cbc67e9c5f515ff5d4bda6e471e7bb2e71c
ELHAJY <elhajydriss@gmail.com>
2023-07-26 14:01:24 -0700

    alx-low_level_programming

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * function should work exactly like strcmp
+ *
+ * @s1: 1 input char
+ * @s2: 2 input char
+ *
+ * Return: deference
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, d = 0;
+
+	while (s1[i])
+	{
+		d = (int)*s1[i] - (int)*s2[i];
+		i++;
+	}
+	return (d);
+}
