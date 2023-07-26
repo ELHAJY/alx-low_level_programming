5963d558db117df44237d50f084dd85bc9f9f806
ELHAJY <elhajydriss@gmail.com>
2023-07-26 12:48:34 -0700

    alx-low_level_programming

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates two strings.
+ *
+ * @dest:first character input
+ * @src: seconde character input
+ * @n: int input
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int v, w, i;
+
+	for (v = 0; dest[v]; v++)
+		;
+	for (w = 0; src[w]; w++)
+		;
+	if (n < w)
+		w = n;	
+	for (i = 0; w > i; i++ )
+	{
+		dest[v++] = src[i];
+		w++;
+	}
+	return (dest);
+}
