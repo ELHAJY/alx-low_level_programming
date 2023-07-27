069d808b6cca55b54efe80ba0d62df9fa2f593a0
ELHAJY <elhajydriss@gmail.com>
2023-07-27 15:41:55 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,25 @@
 #include "main.h"
-
 /**
  * *cap_string - function that capitalizes all words of a string.
  *
  * @ch: input string.
  *
- * Return: capitalizes all words of a string.
  */
-
 char *cap_string(char *ch)
 {
-		int v = 0;
+	int v;
 
-	while (ch[v] != '\0')
+	for (v = 0; ch[v] != '\0'; v++)
 	{
 		if (ch[v] >= 97 && ch[v] <= 122)
-			if (v == 0)
-			{
-				s[i] -= 32;
-			}
-			if (ch[v - 1] == 32 || ch[v - 1] == 9 || s[v - 1] == 10 ||
-				ch[v - 1] == 44 || ch[v - 1] == 59 || ch[v - 1] == 46 ||
-				ch[v - 1] == 33 || ch[v - 1] == 63 || ch[v - 1] == 34 ||
-				ch[v - 1] == 40 || ch[v - 1] == 41 || ch[v - 1] == 123 ||
-				ch[v - 1] == 124)
+		{
+			ch[v] -= 32;
+			if (ch[v - 1] == ' ' || ch[v - 1] == '\n' || ch[v - 1] == '\t')
 			{
 				ch[v] -= 32;
 			}
 		}
-		v++;
+		else
+	}
 	return (ch);
 }
