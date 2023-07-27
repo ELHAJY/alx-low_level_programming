5a37c38e5e71aba522d7f9a6ea1db679c56d5e16
ELHAJY <elhajydriss@gmail.com>
2023-07-27 16:04:06 -0700

    alx-low_level_programming

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,9 +14,11 @@ char *cap_string(char *ch)
 	for (v = 0; ch[v] != '\0'; v++)
 	{
 		if (ch[v] >= 97 && ch[v] <= 122 && (ch[v - 1] == ' ' || ch[v - 1] == '\n' || ch[v - 1] == '\t'))
-		{
-			ch[v] -= 32;
-		}
+			{
+				{
+				ch[v] -= 32;
+				}
+			}
 	}
 	return (ch);
 }
