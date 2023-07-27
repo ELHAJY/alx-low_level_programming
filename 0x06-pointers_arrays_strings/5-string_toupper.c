ddc2119b679191b7e1b23402c409a1dee4222e19
ELHAJY <elhajydriss@gmail.com>
2023-07-27 12:05:59 -0700

    alx-low_level_programming

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * 
+ * @ch: imput string
+ *
+ * Return: string to uppercase.
+ */
+
+char *string_toupper(char *ch)
+{
+	int v;
+
+	for (v = 0; ch[v] != '\0'; v++)
+	{
+		if (ch[v] >= 97 && ch[v] <= 122)
+		{
+			ch[v] += 32;
+		}
+	}
+	return (ch);
+}
