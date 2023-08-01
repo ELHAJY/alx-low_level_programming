daf2442c62a76f5adc41fd4b8d06918f2d1796f9
ELHAJY <elhajydriss@gmail.com>
2023-08-01 15:33:52 -0700

    alx-low_level_programming

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - function that gets the length of a prefix substring.
  * @s: initial segment.
  * @accept: accepted bytes.
- * Return: number of bytes in the initial 
+ * Return: number of bytes in the initial
  * segment of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
