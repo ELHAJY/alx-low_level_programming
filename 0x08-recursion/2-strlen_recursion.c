7766be0450cbc549acf1da93b6506c2f9d3473cb
ELHAJY <elhajydriss@gmail.com>
2023-08-02 14:28:30 -0700

    alx-low_level_programming

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - function that returns the length of a string.
  *
- * @s: input char 
+ * @s: input char
  *
  * Return: length of a string.
 */
@@ -11,7 +11,7 @@
 int _strlen_recursion(char *s)
 {
 	int v = 0;
-	
+
 	if (*s > '\0')
 	{
 		v += _strlen_recursion(s + 1) + 1;
