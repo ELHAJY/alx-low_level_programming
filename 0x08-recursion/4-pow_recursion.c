238b6fbbaf009db00bd9d549b6a15540bbd0d589
ELHAJY <elhajydriss@gmail.com>
2023-08-02 16:07:30 -0700

    alx-low_level_programming

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,10 +12,12 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y < 0 || (y == 0 && x == 0))
 		return (-1);
 	else if (y == 0)
 		return (1);
+	else if (x == 0)
+		return (0);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
