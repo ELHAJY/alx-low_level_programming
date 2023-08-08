010537e0184a29426d0663ae7350d59df45c5e82
ELHAJY <elhajydriss@gmail.com>
2023-08-08 12:01:17 -0700

    alx-low_level_programming

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two numbers.
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0-succes, 1 if error
+ */
+
+int main(int argc, char *argv)
+{
+	int m;
+
+	if (argc == 3)
+	{
+		m = atio(argv[2]) * atio(argv[1]);
+		printf("%d\n", m);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+
+}
