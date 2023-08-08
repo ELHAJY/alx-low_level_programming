1a8285d955b612befafcfea7c74f03fa6cdc3966
ELHAJY <elhajydriss@gmail.com>
2023-08-08 10:56:57 -0700

    alx-low_level_programming

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <sdtio.h>
+
+/**
+ * main - program that prints the number of arguments passed into it.
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * return: 0
+ *
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	print("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
