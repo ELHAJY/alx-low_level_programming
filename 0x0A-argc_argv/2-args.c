941725ca69960fb6d4c391bd4a1afc7a1e748ec1
ELHAJY <elhajydriss@gmail.com>
2023-08-08 11:46:53 -0700

    alx-low_level_programming

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all arguments it receives.
+ *
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char const **argv)
+{
+	int v;
+
+	for (v = 0; v < argc; v++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
