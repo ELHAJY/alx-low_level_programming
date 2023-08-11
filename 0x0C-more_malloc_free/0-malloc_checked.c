f564f986c147696ff15cc0fbd1f5b0ec079f3c3f
ELHAJY <elhajydriss@gmail.com>
2023-08-11 14:36:32 -0700

    alx-low_level_programming

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *malloc_checked - function that allocates memory using malloc.
+ *@b: number of memory
+ *Return: b
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *v;
+
+v = malloc(b);
+        if (!v)
+        {
+                exit(98);
+        }
+
+return (b);
+}
