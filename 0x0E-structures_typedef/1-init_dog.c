91033e1514c61fcdeee38160c5bc229ef75a15a7
ELHAJY <elhajydriss@gmail.com>
2023-08-15 16:05:47 -0700

    alx-low_level_programming

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,9 +2,10 @@
 
 /**
  * init_dog - function that initialize a variable of type struct dog.
- * @name, type = char *
- * @age, type = float
- * @owner, type = char *
+ * @name: type = char *
+ * @age: type = float
+ * @owner: type = char *
+ * @d: struct
  *
  */
 
@@ -12,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d ->name = name;
-		d ->age = age;
-		d ->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
