65d4c280ffdb79f2110d07d21f129b225da5a981
ELHAJY <elhajydriss@gmail.com>
2023-08-16 18:23:59 -0700

    alx-low_level_programming

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,15 @@
 #include "3-calc.h"
 
 /**
- * main - program that perfroms simple operations
+ * main - should contain your main function only.
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: integer
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
+	int ar1, ar2, res;
 	char o;
 	int (*func)(int, int);
 
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	ar1 = atoi(argv[1]);
+	ar2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -32,15 +32,15 @@ int main(int argc, char *argv[])
 
 	o = *argv[2];
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((o == '/' || o == '%') && ar2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
+	res = func(ar1, ar2);
 
-	printf("%d\n", result);
+	printf("%d\n", res);
 
 	return (0);
 }
