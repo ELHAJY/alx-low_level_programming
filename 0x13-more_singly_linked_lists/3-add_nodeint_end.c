e2af15c76006acbba01d046adce56b205ef98a5c
ELHAJY <elhajydriss@gmail.com>
2023-08-29 16:16:12 -0700

    alx-low_level_programming

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint_end - function that adds a new node at the end of a listint_t list.
+ * add_nodeint_end - function that adds
+ *a new node at the end of a listint_t list.
  * @head: double pointer to the beginning of the list
  * @n: int to add to the list
  *
